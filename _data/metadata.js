d05d33f226e53f2e92af498466a4dca2ba39403d
Cade Parker <cademparker@gmail.com>
2026-03-09 09:16:01 -0500

    fix url to actually point to the blog

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,13 +1,13 @@
 export default {
 	title: '50 Compact Discs',
-	url: 'https://cadeparker.me/50cds',
+	url: 'https://blog.cadeparker.me',
 	language: 'en',
 	description:
 		'Gathering-place of all my thoughts on art, music, movies and probably some other stuff. Not really a blog dedicated to code or anything, but there may be some stuff bleed in there on interesting AV solutions I find, or whatnot.',
 	author: {
 		name: 'Cade Parker',
 		email: 'cademparker@proton.me',
-		url: 'https://cadeparker.me',
+		url: 'https://blog.cadeparker.me',
 	},
 	pageSize: 24,
 	hiddenTags: ['all', 'posts'],
